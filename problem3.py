ce23ea8789ff46d4074728cc2f2e5ffbe838a293
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-09-29 12:37:08 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -26,3 +26,38 @@ Enter an integer=>4
 Enter an integer=>2
 2,4,5 do not form a Pythagorean triple
 """
+import math
+n1 = input("Enter 1st number ")
+n2 = input("Enter 2nd number ")
+n3 = input("Enter 3rd number ")
+n1 = float(n1)
+n2 = float(n2)
+n3 = float(n3)
+if n1 < n2 and n1 < n3:
+    a = n1
+    if n2 < n3:
+        b = n2
+        c = n3
+    else:
+        b = n3
+        c = n2
+elif n2 < n1 and n2 < n3:
+    a = n2
+    if n1 < n3:
+        b = n1
+        c = n3
+    else:
+        b = n3
+        c = n1
+else:
+    a = n3
+    if n2 < n1:
+        b = n2
+        c = n1
+    else:
+        b = n1
+        c = n2
+if math.sqrt(a * a + b * b) == c:
+    print (str(a) + "," + str(b) + "," + str(c) + " form a Pythagorean Triple")
+else:
+     print (str(a) + "," + str(b) + "," + str(c) + " do not form a Pythagorean Triple")
