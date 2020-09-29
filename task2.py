3ae5ac7934fab9a541f4dcb4d1685c24f813da88
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-09-29 11:47:10 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -18,3 +18,16 @@ Example:
 Enter a number: 8
 8 is only a perfect cube.
 """
+import math
+ip = input("Enter number ")
+ip = int(ip)
+ps = math.sqrt(ip)
+pc = math.ceil(ip**(1/3))
+if int(ps + 0.5) ** 2 == ip and math.pow(pc,3) == ip : 
+    print("is both a perfect square and a perfect cube.")
+elif int(ps + 0.5) ** 2 == ip:
+    print ("is only a perfect square.")
+elif math.pow(pc,3) == ip:
+    print("is only a perfect cube.")
+    else:
+    print ("None")
