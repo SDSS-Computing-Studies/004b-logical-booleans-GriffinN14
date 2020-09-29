6858bdbf0329f999b7e3ea120a6b761a89db42b8
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-09-29 12:24:36 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -23,3 +23,17 @@ Enter a number: 4
 Enter another number: 25
 4 is not a factor of 25
 """
+n1 = input("Input number 1 ")
+n2 = input("Input number 2 ")
+n1 = float(n1)
+n2 = float(n2)
+if n1 > n2:
+    if n1 / n2 == int(n1 / n2):
+        print ("is a factor of " + str(n2))
+    else:
+        print ("is not a factor of " + str(n2))
+else:
+    if n2 / n1 == int(n2 / n1):
+        print ("is a factor of " + str(n1))
+    else:
+        print ("is not a factor of " + str(n1))
